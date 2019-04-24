f3ed6a3f0081a1090c2a5bfa22082bcaa6abd14b
davidlkang <david.kang@outlook.de>
2019-04-24 12:45:47 +0200

    edited the application challenge

--- code-application-challenge/code-application-challenge-codieren.php
+++ code-application-challenge/code-application-challenge-codieren.php
@@ -14,8 +14,8 @@ $CX = ($CX + 1) / 2;
 settype($CX, "integer");
 
 if($CX == 22173){
-	$J = 1; 
-	$Y0 = $Y0 + 44346;} 
+	$J = 1;
+	$Y0 = $Y0 + 44346;}
 else{
 	$J = 0;}
 
@@ -29,7 +29,7 @@ $p_teile = explode("~", $p_Split);
 for([$C0=0, $C1=1, $Z=0, $C2=0]; ;[$CX--, $Z++]){
 	$BX = 16;
 	if($CX > 0){
-		$AX = strrev(hex2bin($i[$Z])); 
+		$AX = strrev(hex2bin($i[$Z]));
 		$AX = bin2hex($AX);
 		for( ; ;[$BX--, $C0++, $C1++, $C2++]){
 			if($BX >= 0){
@@ -37,19 +37,21 @@ for([$C0=0, $C1=1, $Z=0, $C2=0]; ;[$CX--, $Z++]){
 					$BP =  $p_teile[$C1].$p_teile[$C0];
 					$BP = str_pad($BP, 4 ,"0", STR_PAD_LEFT);
 					$BP = hexdec($BP);
+
 					if($CX > $BP){
 						$BP = $BP - $CX;
 						$BP = dechex($BP);
-						$BP = substr($BP, 4, 4);} 
+						$BP = substr($BP, 4, 4);}
 					else{
-						$BP = $BP - $CX; 
+						$BP = $BP - $CX;
 						$BP = dechex($BP);}
-					
+
 					$BP = str_pad($BP, 4 ,"0", STR_PAD_LEFT);
-					$BP0 = chunk_split($BP,2,"~");
+					$BP0 = chunk_split($BP, 2, "~");
 					$BP0 = explode("~", $BP0);
 					$BP1 = $BP0[1];
 					$V[$C2] = $BP1;}
+
 				else {
 					if($C2 == 9){
 						$C2 = 0;}
@@ -58,28 +60,32 @@ for([$C0=0, $C1=1, $Z=0, $C2=0]; ;[$CX--, $Z++]){
 					$BP = $V[$C2 + 1] . $V[$C2];
 					$BP = str_pad($BP, 4 ,"0", STR_PAD_LEFT);
 					$BP = hexdec($BP);
+
 					if($CX > $BP){
 						$BP = $BP - $CX;
 						$BP = dechex($BP);
-						$BP = substr($BP,4,4);} 
+						$BP = substr($BP, 4, 4);}
 					else{
 						$BP = $BP-$CX;
 						$BP= dechex($BP);}
+
 					$BP = str_pad($BP, 4 ,"0", STR_PAD_LEFT);
-					$BP0 = chunk_split($BP,2,"~");
+					$BP0 = chunk_split($BP, 2, "~");
 					$BP0 = explode("~", $BP0);
 					$BP1 = $BP0[1];
 					$V[$C2] = $BP1;}
-				$AX = str_pad($AX, 4 ,"0", STR_PAD_LEFT);
+				$AX = str_pad($AX, 4, "0", STR_PAD_LEFT);
 				$AX = hexdec($AX);
 				$BP = hexdec($BP);
 				$AX = $AX ^ $BP;
 				$AX = $AX ^ $CX;
 				$AX = dechex($AX);
 				$AX = str_pad($AX, 4 ,"0", STR_PAD_LEFT);}
+
 			else {
 				$AX = hex2bin($AX);
 				$AX = strrev($AX);
+
 				if(empty($F)){
 					$F = $AX;}
 				else {
@@ -87,8 +93,10 @@ for([$C0=0, $C1=1, $Z=0, $C2=0]; ;[$CX--, $Z++]){
 				break;}}}
 	else {
 		break;}}
+
 if($J == 1){
 	goto LoopAD39;}
+
 file_put_contents("$Outputfilename", $F);
 
 ?></body></html>
